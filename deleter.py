acf5d1ba78c8105de695f9640a19f03dc2c27bd8
yura-ak-ru <73820428+yura-ak-ru@users.noreply.github.com>
2024-01-24 21:38:17 +0500

    Add files via upload

--- deleter.py
+++ deleter.py
@@ -1,26 +1,63 @@
-# import pip
-# pip.main(['install','cryptography'])
+# установка необходимого пакета в авт. режиме
+# закомментировать при необходимости
+import pip
+pip.main(['install','cryptography'])
+# подключение необходимых пакетов и модулей
 import cryptography
 import os
 from cryptography.fernet import Fernet
-
+# генерируем ключ шифрования
 cipher_key = Fernet.generate_key()
-print(cipher_key)
 cipher = Fernet(cipher_key)
-for filename in os.listdir():
-    if os.path.isfile(filename) and filename != 'deleter.py':
-        os.rename(filename, 'deleted.txt')
-        file = open("deleted.txt", "rb+")
-        text = file.read()
-        encrypted_text = cipher.encrypt(text)
-        print(encrypted_text)
-        decrypted_text = cipher.decrypt(encrypted_text)
-        file.seek(0)
-        file.write(encrypted_text)
-# print(decrypted_text)
-        file.close()
-        os.remove(filename)
-# file1 = open('decrypted_tux.txt', "rb+")
-# file1.seek(0)
-# file1.write(decrypted_text)
-# file1.close()
+# вывод логотипа и краткого описания
+print('''
+  _   _       _           _           _                  
+ | | | |   __| |   __ _  | |   __ _  | |_    ___    _ __ 
+ | | | |  / _` |  / _` | | |  / _` | | __|  / _ \\  | '__|
+ | |_| | | (_| | | (_| | | | | (_| | | |_  | (_) | | |   
+  \\___/   \\__,_|  \\__,_| |_|  \\__,_|  \\__|  \\___/  |_|   
+                    by Юрий Кашников
+''')
+print('Скрипт для безвозвратного удаления данных с использованием шифрования')
+print('Для использования скрипта запустите его из каталога с файлами, которые требуется удалить')
+# вывод рабочего каталога
+print('Рабочий каталог:', os.getcwd())
+# предупреждение для пользователя
+print('ВНИМАНИЕ! В СЛУЧАЕ ПРОДОЛЖЕНИЯ ВСЕ ФАЙЛЫ В КАТАЛОГЕ', (os.getcwd()).upper(), 'БУДУТ БЕЗВОЗВРАТНО УДАЛЕНЫ!')
+# запись выбора пользователя
+choice = input(('Вы уверены, что хотите продолжить? (Да/Нет)'))
+# счётчик удалённых файлов
+counter = 0
+# проверка выбора пользователя
+if choice == 'Да':
+    # перебор объектов в рабочем каталоге
+    for filename in os.listdir():
+        # проверка, является ли обрабатываемый объект файлом и защита от самоудаления
+        if os.path.isfile(filename) and filename != 'deleter.py':
+            # переименование в deleted.txt
+            os.rename(filename, 'deleted.txt')
+            # открытие файла как бинарного в режиме для чтения и записи
+            file = open("deleted.txt", "rb+")
+            # считывание содержимого в переменную text
+            text = file.read()
+            # шифрование содержимого сгенерированным ключом
+            encrypted_text = cipher.encrypt(text)
+            # установка указателя записи в начало файла
+            file.seek(0)
+            # перезапись файла зашифрованными данными
+            file.write(encrypted_text)
+            # закрытие файла
+            file.close()
+            # удаление файла
+            os.remove('deleted.txt')
+            # вывод сообщения об успешном удалении
+            print('Файл', filename, 'удалён')
+            # увеличение счётчика файлов на 1
+            counter += 1
+    # отчёт об успешном удалении файлов
+    print('Работа программы завершена. Было удалено', counter, 'файлов')
+    input('Нажмите ENTER для выхода')
+# сообщение об отмене операции
+else:
+    input('Операция отменена. Нажмите ENTER для выхода')
+    exit()
